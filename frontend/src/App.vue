4d9d34598fb362baf382732091d547530f150aba
kseoh2 <kseoh2@wavus.co.kr>
2022-10-09 17:48:24 +0900

    frontend commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <MainMap />
+    <SideBar class="side-bar" />
+  </div>
+</template>
+
+<script>
+import MainMap from './components/MainMap.vue';
+import SideBar from './components/SideBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainMap,
+    SideBar
+  }
+}
+</script>
+
+<style>
+#app {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.side-bar {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="side-bar-wrapper">
+    <div class="side-bar">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.side-bar-wrapper {
+  > .side-bar {
+    background-color: #000000;
+    opacity: 0.5;
+    width: 500px;
+    height: 100%;
+  }
+}
+
+</style>
