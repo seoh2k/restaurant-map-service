fb4998909c566071ba3fa3a47cb89e71d60b2be9
kseoh2 <kseoh2@wavus.co.kr>
2022-10-10 17:02:38 +0900

    SideBar css 구현

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  // transpileDependencies: true
+
+  outputDir: "../src/main/resources/static",  // 빌드 타겟 디렉토리
+  devServer: {
+    proxy: {
+      '/api': {
+        // '/api' 로 들어오면 포트 6060(스프링 서버)로 보낸다
+        target: 'http://localhost:6060',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  }
 })
